111e97a1f012c98eb5ff23a82afa723d78db6a99
ajgerim <sanzharvaisov@gmail.com>
2024-05-26 02:43:16 +0500

    test commit 2

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,33 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Opt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        MyGraph<String> graph = new MyGraph<>();
 
-        // Press Ctrl+R or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        graph.addVertex("Almaty");
+        graph.addVertex("Astana");
+        graph.addVertex("Shymkent");
+        graph.addVertex("Pavlodar");
+        graph.addVertex("Oral");
 
-            // Press Ctrl+D to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Cmd+F8.
-            System.out.println("i = " + i);
-        }
+        graph.addEdge("Almaty","Astana");
+        graph.addEdge("Almaty","Shymkent");
+        graph.addEdge("Astana","Pavlodar");
+        graph.addEdge("Astana","Oral");
+
+
+        System.out.println(graph.hasEdge("Almaty", "Astana"));
+        System.out.println(graph.hasEdge("Almaty", "Pavlodar"));
+
+        System.out.println();
+
+        System.out.println(graph.getNeighbors("Almaty"));
+
+        graph.printGraph();
+        System.out.println("_____________________________");
+        graph.removeEdge("Astana", "Oral");
+
+        graph.printGraph();
+
+
+
     }
 }
--- src/MyGraph.java
+++ src/MyGraph.java
@@ -1,15 +1,52 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class MyGraph<Vertex> {
     private Map<Vertex, List<Vertex>> list;
+
     public MyGraph(){
-        list = new HashMap<Vertex, List<Vertex>>();
+        list = new HashMap<>();
     }
 
-    public void addVertex(Vertex vertex){
-        list.put(vertex, new ArrayList<Vertex>());
+    public void addVertex(Vertex vertex) {
+        list.put(vertex, new ArrayList<>());
     }
+
+    public void addEdge(Vertex source, Vertex destination) {
+        checkVertex(source);
+        checkVertex(destination);
+        list.get(source).add(destination);
+        list.get(destination).add(source);
+    }
+
+    private void checkVertex(Vertex vertex) {
+        if (!list.containsKey(vertex))
+            throw new IndexOutOfBoundsException("Vertex does not exist");
+    }
+
+    public boolean hasEdge(Vertex source, Vertex destination) {
+        checkVertex(source);
+        checkVertex(destination);
+        List<Vertex> neighbors = getNeighbors(source);
+        return neighbors != null && neighbors.contains(destination);
+    }
+
+    public List<Vertex> getNeighbors(Vertex vertex) {
+        List<Vertex> neighbors = list.get(vertex);
+        return neighbors;
+    }
+
+    public void printGraph() {
+        for(Vertex vertex : list.keySet()) {
+            System.out.println("Vertex " + vertex + " connected to " + getNeighbors(vertex));
+        }
+    }
+
+    public void removeEdge(Vertex source, Vertex destination) {
+        checkVertex(source);
+        checkVertex(destination);
+
+        list.get(source).remove(destination);
+        list.get(destination).remove(source);
+    }
+
 }
